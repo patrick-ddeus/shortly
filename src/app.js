e1cddce0dc2f7afe0eae6a692296bc31ff19e2ba
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-05-21 11:18:36 -0300

    fix: dump.sql type

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import IndexRouter from "./routes/index.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(IndexRouter);
 
 const porta = process.env.PORT || 5000;
